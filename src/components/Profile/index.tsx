0043169e6213dc4f21e784f95cf41826687680ae
assoares <alexandressoares383@gmail.com>
2021-02-24 00:15:10 -0300

    creation of the home.module.css file

---
+++ src/components/Profile/index.tsx
@@ -0,0 +1,11 @@
+export default function Profile() {
+  return (
+    <div>
+      <img src="https://avatars.githubusercontent.com/u/15836394?s=460&u=70673f20d41e1ee784577c36affb639d453a3552&v=4" alt="Alexandre Soares"/>     
+      <div>
+        <strong>Alexandre Soares</strong>
+        <p>Level 1</p>
+      </div>
+    </div>
+  )
+}
