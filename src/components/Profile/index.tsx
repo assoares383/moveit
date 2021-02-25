537944ce10b3a90d190c0fe3a385f6455633e552
assoares <alexandressoares383@gmail.com>
2021-02-24 21:59:41 -0300

    countdown

--- src/components/Profile/index.tsx
+++ src/components/Profile/index.tsx
@@ -1,10 +1,15 @@
+import styles from '../../styles/components/Profile.module.css';
+
 export default function Profile() {
   return (
-    <div>
+    <div className={styles.profileContainer}>
       <img src="https://avatars.githubusercontent.com/u/15836394?s=460&u=70673f20d41e1ee784577c36affb639d453a3552&v=4" alt="Alexandre Soares"/>     
       <div>
         <strong>Alexandre Soares</strong>
-        <p>Level 1</p>
+        <p>
+          <img src="icons/level.svg" alt="Level" />
+          Level 1
+        </p>
       </div>
     </div>
   )
