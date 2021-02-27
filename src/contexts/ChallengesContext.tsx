21d483816fa9f74271abe59842c5f3bf6e7180cc
assoares <alexandressoares383@gmail.com>
2021-02-27 18:30:22 -0300

    web notification

--- src/contexts/ChallengesContext.tsx
+++ src/contexts/ChallengesContext.tsx
@@ -1,4 +1,4 @@
-import { createContext, useState, ReactNode } from 'react';
+import { createContext, useState, ReactNode, useEffect } from 'react';
 import challenges from '../../challenges.json';
 
 interface Challenge {
@@ -33,19 +33,29 @@ export function ChallengesProvider({ children }: ChallengesProviderProps) {
 
   const experienceToNextLevel = Math.pow((level + 1) * 4, 2);
 
+  useEffect(() => {
+    Notification.requestPermission();
+  }, []);
+
   function levelUp() {
     setLevel(level + 1);
   }
 
   function startNewChallenge() {
-  const randomChallengeIndex = Math.floor(Math.random() * challenges.length);
-  const challenge = challenges[randomChallengeIndex];
+    const randomChallengeIndex = Math.floor(Math.random() * challenges.length);
+    const challenge = challenges[randomChallengeIndex];
 
-  setActiveChallenge(challenge);
+    setActiveChallenge(challenge);
+
+    if (Notification.permission === 'granted') {
+      new Notification('Novo desafio', {
+        body: `Valendo ${challenge.amount}xp!`
+      })
+    }
   }
 
   function resetChallenge() {
-  setActiveChallenge(null);
+    setActiveChallenge(null);
   }
 
   function completeChallenge() {
